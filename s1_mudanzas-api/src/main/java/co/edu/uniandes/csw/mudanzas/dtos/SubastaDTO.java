b810b30999b664ec50a9c812de9aa09926a4fc0e
je.osorio <je.osorio@uniandes.edu.co>
2019-02-10 22:17:47 -0500

    clases implementadas por aj.gonzalezt: OfertaDTO, SubastaDTO, OfertaProveedorReaource y servicios de SubastasProveedorResource, SubastasUsuarioResource

--- s1_mudanzas-api/src/main/java/co/edu/uniandes/csw/mudanzas/dtos/SubastaDTO.java
+++ s1_mudanzas-api/src/main/java/co/edu/uniandes/csw/mudanzas/dtos/SubastaDTO.java
@@ -10,5 +10,41 @@ package co.edu.uniandes.csw.mudanzas.dtos;
  * @author estudiante
  */
 public class SubastaDTO {
+  
+    private Long idSubasta;
     
+    private double valorInicial;
+    
+    private double valorFinal;
+    
+    public  SubastaDTO()
+    {
+        
+    }
+
+    public Long getIdSubasta() {
+        return idSubasta;
+    }
+
+    public void setIdSubasta(Long idSubasta) {
+        this.idSubasta = idSubasta;
+    }
+
+    public double getValorInicial() {
+        return valorInicial;
+    }
+
+    public void setValorInicial(double valorInicial) {
+        this.valorInicial = valorInicial;
+    }
+
+    public double getValorFinal() {
+        return valorFinal;
+    }
+
+    public void setValorFinal(double valorFinal) {
+        this.valorFinal = valorFinal;
+    }
+    
+            
 }
