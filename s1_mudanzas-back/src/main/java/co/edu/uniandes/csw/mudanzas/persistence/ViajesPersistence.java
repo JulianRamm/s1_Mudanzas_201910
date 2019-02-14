a6e9a97029d977e6bd0eee43273eb77d2833a1a0
LuisMiguel <lm.gomezl@uniandes.edu.co>
2019-02-13 19:34:30 -0500

    Se arreglo bug con arquilian, mala referencia a base de datos arreglada

--- s1_mudanzas-back/src/main/java/co/edu/uniandes/csw/mudanzas/persistence/ViajesPersistence.java
+++ s1_mudanzas-back/src/main/java/co/edu/uniandes/csw/mudanzas/persistence/ViajesPersistence.java
@@ -6,10 +6,12 @@
 package co.edu.uniandes.csw.mudanzas.persistence;
 
 import co.edu.uniandes.csw.mudanzas.entities.ViajesEntity;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import java.util.logging.Logger;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 /**
  *
  * @author je.osorio
@@ -17,12 +19,43 @@ import javax.persistence.PersistenceContext;
 @Stateless
 public class ViajesPersistence {
     
-    private static final Logger LOGGER=Logger.getLogger(ViajesPersistence.class.getName());
-    
-    @PersistenceContext(unitName="mudazasPU")
+    private static final Logger LOGGER = Logger.getLogger(ViajesPersistence.class.getName());
+
+    /**
+     * El manejador de la base de datos.
+     */
+    @PersistenceContext(unitName = "mudanzasPU")
     protected EntityManager em;
-    public ViajesEntity create(ViajesEntity viajesEntity){
-        em.persist(viajesEntity);
-        return viajesEntity;
+
+    /**
+     * Crea un viaje en la BD
+     *
+     * @param viaje - la instancia del viaje que se quiere crear
+     * @return el viaje creado con algunas variaciones adaptadas a la BD como
+     * por ejemplo el id.
+     */
+    public ViajesEntity create(ViajesEntity viaje) {
+        em.persist(viaje);
+        return viaje;
+    }
+
+    /**
+     * Encuentra a un viaje en la BD con su id
+     *
+     * @param viajeId el id del viaje que estamos buscando
+     * @return la entidad del viaje encontrado
+     */
+    public ViajesEntity find(Long viajeId) {
+        return em.find(ViajesEntity.class, viajeId);
+    }
+
+    /**
+     * Encuentra todos los viajes que existen en la BD
+     *
+     * @return una lista con todos los viajes.
+     */
+    public List<ViajesEntity> findAll() {
+        TypedQuery<ViajesEntity> query = em.createQuery("select u from ViajesEntity u", ViajesEntity.class);
+        return query.getResultList();
     }
 }
