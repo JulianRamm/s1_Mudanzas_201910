1f5c5f523883c4027862b52b4b8c0a20d28caa91
LuisMiguel <lm.gomezl@uniandes.edu.co>
2019-03-03 13:17:02 -0500

    .sql

--- s1_mudanzas-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s1_mudanzas-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -54,8 +54,6 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorVehiculoResource.class);
-        resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorViajesResource.class);
-        resources.add(co.edu.uniandes.csw.mudanzas.resources.OfertaProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.SubastasProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.SubastasUsuarioResource.class);
@@ -66,6 +64,8 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.mudanzas.resources.VehiculosProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ViajesCargaResource.class);
 
-    }    
+    }
+
+    
 }
 
