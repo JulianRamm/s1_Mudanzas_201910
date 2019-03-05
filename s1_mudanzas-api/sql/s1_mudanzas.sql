fd26211a894d4c0ac893716b8fff49a72fd0769c
LuisMiguel <lm.gomezl@uniandes.edu.co>
2019-03-04 21:33:22 -0500

    .sql terminado luismiguel

--- s1_mudanzas-api/sql/s1_mudanzas.sql
+++ s1_mudanzas-api/sql/s1_mudanzas.sql
@@ -18,3 +18,13 @@ insert into TarjetaDeCreditoEntity (id, nombreTarjeta, numeroSerial, codigoSegur
 insert into TarjetaDeCreditoEntity (id, nombreTarjeta, numeroSerial, codigoSeguridad, titularCuenta, fechaVencimiento, usuario_id) values (10004, 'Tarjeta secundaria', '123456789103', 952, 'Luis Miguel Gomez L', '02/10/2020', 10001); 
 insert into TarjetaDeCreditoEntity (id, nombreTarjeta, numeroSerial, codigoSeguridad, titularCuenta, fechaVencimiento, usuario_id) values (10005, 'Tarjeta primaria', '123456789104', 953, 'Julian Osorio', '02/10/2021', 10002); 
 insert into TarjetaDeCreditoEntity (id, nombreTarjeta, numeroSerial, codigoSeguridad, titularCuenta, fechaVencimiento, usuario_id) values (10006, 'Tarjeta secundaria', '123456789105', 954, 'Julian Osorio', '02/10/2021', 10002);
+
+/*  @Andres Gonzalez   
+    Para el query de subasta: pon a los usuarios de 
+    id 10001, y 10002 en cada subasta... ponle dos 
+    subastas a cada uno. Usuario se llama usuario_id*/
+
+/*  @Julian Osorio   
+    Para el query de carga: pon a los usuarios de 
+    id 10001, y 10002 en cada subasta... ponle dos 
+    cargas a cada uno. Usuario se llama usuario_id*/
