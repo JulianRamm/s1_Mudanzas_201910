523be4d06cf000e9e0fc872c4ede3b7352a79906
aj.gonzalezt <aj.gonzalezt@uniandes.edu.co>
2019-03-08 21:57:40 -0500

    issues & documentar

--- s1_mudanzas-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s1_mudanzas-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -45,8 +45,8 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.mudanzas.mappers.WebApplicationExceptionMapper.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorVehiculoResource.class);
+        resources.add(co.edu.uniandes.csw.mudanzas.resources.OfertaProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ProveedorResource.class);
-        resources.add(co.edu.uniandes.csw.mudanzas.resources.SubastasProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.UsuarioResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.VehiculoDiaResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.VehiculosProveedorResource.class);
