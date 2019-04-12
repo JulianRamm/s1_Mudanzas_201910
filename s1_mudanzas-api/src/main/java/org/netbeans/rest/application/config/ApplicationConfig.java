323df6a53166922c005ad62246f71c4cbf888a86
LuisMiguel <lm.gomezl@uniandes.edu.co>
2019-04-11 19:46:40 -0500

    config

--- s1_mudanzas-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s1_mudanzas-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -43,10 +43,13 @@ private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(co.edu.uniandes.csw.mudanzas.mappers.ExceptionMapperA.class);
         resources.add(co.edu.uniandes.csw.mudanzas.mappers.WebApplicationExceptionMapper.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.CargasUsuarioResource.class);
+        resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorVehiculoResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorViajesResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.SubastasProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.UsuarioResource.class);
+        resources.add(co.edu.uniandes.csw.mudanzas.resources.VehiculoDiaResource.class);
+        resources.add(co.edu.uniandes.csw.mudanzas.resources.VehiculosProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ViajesCargaResource.class);
     }
 }
