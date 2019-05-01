3b2aa0c72ab58e03a160b870a6742082c0b3a67c
Samuel Bernal Neira <s.bernaln@uniandes.edu.co>
2019-05-01 16:17:22 -0500

    Config

--- s1_mudanzas-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s1_mudanzas-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -42,7 +42,9 @@ private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(co.edu.uniandes.csw.mudanzas.mappers.BusinessLogicExceptionMapper.class);
         resources.add(co.edu.uniandes.csw.mudanzas.mappers.ExceptionMapperA.class);
         resources.add(co.edu.uniandes.csw.mudanzas.mappers.WebApplicationExceptionMapper.class);
+        resources.add(co.edu.uniandes.csw.mudanzas.resources.CargasUsuarioResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorProveedorResource.class);
+        resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorVehiculoResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.ConductorViajesResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.OfertaProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.OfertasSubastaResource.class);
@@ -50,6 +52,7 @@ private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(co.edu.uniandes.csw.mudanzas.resources.SubastaResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.SubastasProveedorResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.SubastasUsuarioResource.class);
+        resources.add(co.edu.uniandes.csw.mudanzas.resources.TarjetasUsuarioResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.UsuarioResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.VehiculoDiaResource.class);
         resources.add(co.edu.uniandes.csw.mudanzas.resources.VehiculosProveedorResource.class);
